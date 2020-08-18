5098de8d6a86dde7417ace84978cbd5b233ccfd0
WilkRhu <wilk.caetano@gmail.com>
2020-08-18 13:08:32 -0300

    Modificação arquivo rota api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+//============================ Rota Api Loja ======================//
+    Route::get("/loja", "lojaController@index");
+    Route::get("/loja/{id}", "lojaController@show");
+    Route::post("/loja", "lojaController@store");
+    Route::put("/loja/{id}", "lojaController@update");
+    Route::delete("/loja/{id}", "lojaController@destroy");
+//==================================================================//
+
+
